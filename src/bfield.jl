51cc3e2aed590595dd6fb699d62878151abe8c75
Dale-Black <djblack@uci.edu>
2023-05-24 09:13:31 -0700

    conver to pyint before passing

--- src/bfield.jl
+++ src/bfield.jl
@@ -1,8 +1,9 @@
 using NIfTI
 
 function bfield_correction(image_path, mask_path; spline_order=3, num_control_points=4)
-	spline_order, num_control_points = pylist(spline_order), pylist(num_control_points)
-	
+	spline_order = pyint(spline_order)
+	num_control_points = pyint(num_control_points)
+
 	inputImage = sitk.ReadImage(image_path, sitk.sitkFloat32)
 	image = inputImage
 	
