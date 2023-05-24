ef0a63e437f9604f9c3c5c255e08338e56a2db50
Dale-Black <djblack@uci.edu>
2023-05-24 09:02:47 -0700

    add defaults for spine order & control points

--- src/bfield.jl
+++ src/bfield.jl
@@ -1,12 +1,16 @@
 using NIfTI
 
-function bfield_correction(image_path, mask_path)
+function bfield_correction(image_path, mask_path; spline_order=3, num_control_points=4)
+	spline_order, num_control_points = pylist(spline_order), pylist(num_control_points)
+	
 	inputImage = sitk.ReadImage(image_path, sitk.sitkFloat32)
 	image = inputImage
 	
 	maskImage = sitk.ReadImage(mask_path, sitk.sitkUInt8)
 	
 	corrector = sitk.N4BiasFieldCorrectionImageFilter()
+	corrector.SetSplineOrder(spline_order)
+	corrector.SetNumberOfControlPoints(num_control_points)
 
 	corrected_image = corrector.Execute(image, maskImage)
     log_bias_field = corrector.GetLogBiasFieldAsImage(inputImage)
