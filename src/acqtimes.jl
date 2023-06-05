0a06d2df0b5c9d1b8278b1a25961df043f66d1e0
Helmut Strey <Helmut.Strey@stonybrook.edu>
2023-06-05 14:13:34 -0400

    added file to process DICOM json file to extract aquisition times

---
+++ src/acqtimes.jl
@@ -0,0 +1,20 @@
+import JSON3
+using DelimitedFiles
+
+function acqtimes(jsonfilename)
+    json_string = read(jsonfilename, String)
+    data = JSON3.read(json_string)
+    acqt = Float64.(data["SliceTiming"])*1000
+    slices = collect(1:length(acqt))
+    return slices,acqt
+end
+
+function write_acqtimes(slices,acqt,outfile)
+    io = open(outfile, "w")
+    write(io,"Time,Slice\n")
+    for i in 1:length(acqt)
+        write(io, string(acqt[i])*","*string(slices[i])*"\n")
+    end
+    close(io)
+end
+
