00914bcab3c5face92448b12be9433da7d56c143
wildart <wildart@gmail.com>
2023-09-02 14:39:30 -0400

    more comments

--- examples/denoiser.jl
+++ examples/denoiser.jl
@@ -13,20 +13,29 @@ using Plots
 const DATA_DIR = "../PhantomData"
 # phantomfile = joinpath(DATA_DIR, "BFC_time_series.nii")
 phantomfile = joinpath(DATA_DIR, "104", "104.nii")
-groundtruthfile = "examples/gt_raw.h5"
-# groundtruthfile = "examples/gt_clean.h5"
+#
+# cleaned "ground truth" file corresponding to the above phantom data
+#
+groundtruthfile = joinpath(DATA_DIR, "104", "gt_clean.h5")
+# groundtruthfile = "examples/gt_raw.h5"
 
+#
 # following function loads "ground truth" (GT) phantom data and performs its unit normalization
 # `sim` variable contains GT time series as a 3D tensor with a first dimension as a time series length,
 #  a second dimension is 1, and a third dimension is number of voxels from a masked phantom data
+#
 gt, removeidx, sim, _ = BDTools.load_ground_truth(groundtruthfile)
 
+#
 # following function loads original data, uses mask from the GT phantom to filter
 # unused voxels, and performs unit normalization of the data (minimum & maximum are also returned)
 # `remove` parameter is used to explicitly remove voxels by index
+#
 ori, _ = BDTools.load_phantom(gt, phantomfile, valid_slices=true, remove=removeidx)
 
+#
 # construct a denoiser model
+#
 model = DenoiseNet(BDTools.TrainParameters(; epochs=200); dev=Flux.gpu)
 # and train it on a normalized data
 losses = BDTools.train!(model, sim, ori)
